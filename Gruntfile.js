fb2085d7e4d63fbfabbb42a8ba863f591bec74d5
Serge Poltavski <serge.poltavski@gmail.com>
2015-12-06 01:13:21 +0300

    new plugins added: concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
     'use strict'; // включить строгий режим
-    // автоматически загружать задачи (хотя можно и вручную)
-    // require('load-grunt-tasks')(grunt);
+    // автоматически загружать задачи
+    require('load-grunt-tasks')(grunt);
 
     // директории разработки и деплоя
     var config = {
@@ -13,19 +13,28 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         config: 'config',
         less: {},
+        concat: {
+            dist: {
+                src: ['src/js/*.js'],
+                dest: 'build/js/<%= pkg.name %>.js'
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/js/<%= pkg.name %>.js',
+                src: 'build/js/<%= pkg.name %>.js',
                 dest: 'build/js/<%= pkg.name %>.min.js'
-            }
+            },
         },
         jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
             all: [
                 'Gruntfile.js',
-                ['src/js/*.js']
+                'src/js/*.js'
             ]
         },
         watch: {
@@ -42,9 +51,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     // регистрация задач
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['concat', 'jshint', 'uglify']);
 };
