eca36cd1bb43ec45155abfe51d021b7251ad6107
Serge Poltavski <serge.poltavski@gmail.com>
2015-12-06 00:53:05 +0300

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+    'use strict'; // включить строгий режим
+    // автоматически загружать задачи (хотя можно и вручную)
+    // require('load-grunt-tasks')(grunt);
+
+    // директории разработки и деплоя
+    var config = {
+        app: 'src',
+        dist: 'build'
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        config: 'config',
+        less: {},
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/js/<%= pkg.name %>.js',
+                dest: 'build/js/<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                ['<%= config.app %>/scripts/*.js']
+            ]
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            less: {
+                files: ['<%= config.app %>/styles/{,*/}*.less'],
+                tasks: ['less:dev']
+            },
+            js: {
+                files: ['<%= config.app %>/scripts/{,*/}*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // регистрация задач
+    grunt.registerTask('default', ['uglify']);
+};
