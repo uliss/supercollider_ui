84efed0bb02a5e603dfa1617834f381faf973859
Serge Poltavski <serge.poltavski@gmail.com>
2015-12-07 12:56:29 +0300

    knob template added

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,8 @@ module.exports = function(grunt) {
                 },
                 files: {
                     "build/index.html": ["src/index.jade"],
-                    "build/info.html":  ["src/info.jade"]
+                    "build/info.html":  ["src/info.jade"],
+                    "build/knobs.html":  ["src/knobs.jade"],
                 }
             }
         },
