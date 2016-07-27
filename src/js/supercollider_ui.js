330b58e15bfcedb7dc377c12aa9cfb0a3aba2b2b
Serge Poltavski <serge.poltavski@gmail.com>
2016-07-28 02:28:08 +0300

    modal alerts added

--- src/js/supercollider_ui.js
+++ src/js/supercollider_ui.js
@@ -47,10 +47,31 @@ $(document).ready(function() {
         $(msg[0]).css(msg[1], msg[2]);
     });
 
-    socket.on('/cli/modal/alert', function(msg){
-        $('#ui_modal_alert_title').text(msg.title);
-        $('#ui_modal_alert_text').text(msg.text);
-        $('#ui_modal_alert').modal({});
+    socket.on('/cli/alert', function(msg){
+        var show_error = function(msg) {
+            $('#ui_modal_error_title').text(msg.title);
+            $('#ui_modal_error_text').text(msg.text);
+            $('#ui_modal_error').modal({});
+        };
+
+        var show_ok = function(msg) {
+            $('#ui_modal_ok_title').text(msg.title);
+            $('#ui_modal_ok_text').text(msg.text);
+            $('#ui_modal_ok').modal({});
+        };
+
+        var show_info = function(msg) {
+            $('#ui_modal_info_title').text(msg.title);
+            $('#ui_modal_info_text').text(msg.text);
+            $('#ui_modal_info').modal({});
+        };
+
+        switch(msg.type) {
+            case 'error': show_error(msg); break;
+            case 'ok':    show_ok(msg); break;
+            case 'info':  show_info(msg); break;
+            default: { console.log('unknown alert type: ' + msg.type); }
+        }
     });
 
     // handle redirect
