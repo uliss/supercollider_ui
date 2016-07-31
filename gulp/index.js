92551cad28dc687a8d212c98212076754192f28b
Serge Poltavski <serge.poltavski@gmail.com>
2016-07-31 04:33:00 +0300

    htmllint added

--- gulp/index.js
+++ gulp/index.js
@@ -89,7 +89,8 @@ module.exports.add = function() {
 
     gulp.task('htmllint', function() {
         return gulp.src('./build/*.html')
-        .pipe(htmllint({}, htmllintReporter));
+        .pipe(htmllint({
+        }, htmllintReporter));
     });
 
     gulp.task('htmllint:watch', function() {
@@ -99,7 +100,11 @@ module.exports.add = function() {
     function htmllintReporter(filepath, issues) {
         if (issues.length > 0) {
             issues.forEach(function (issue) {
-                gutil.log(gutil.colors.cyan('[gulp-htmllint] ') + gutil.colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + gutil.colors.red('(' + issue.code + ') ' + issue.msg));
+                if(issue.code == "E011") {
+                }
+                else {
+                    gutil.log(gutil.colors.cyan('[gulp-htmllint] ') + gutil.colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + gutil.colors.red('(' + issue.code + ') ' + issue.msg));
+                }
             });
 
             process.exitCode = 1;
