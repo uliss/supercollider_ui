bb4d49be4517f4a66ffc077e2e1396d6ed90842c
Serge Poltavski <serge.poltavski@gmail.com>
2016-08-02 04:07:54 +0300

    server api fix

--- src/js/api.js
+++ src/js/api.js
@@ -6,9 +6,8 @@ function node_path(path) { return "/node" + path; }
 function sc_path(path) { return "/sc" + path; }
 
 var debug = false;
-var server_state = 0;
-var mute_state = 0;
-var server_volume = 0;
+var DEFAULT_STATE = {'boot': false, 'record': false, 'mute': true, 'volume': 0 };
+var server_state = DEFAULT_STATE;
 
 function api_send_to_sc(socket, path) {
     var args = [sc_path(path)].concat(Array.prototype.slice.call(arguments, 2));
@@ -20,67 +19,55 @@ function api_from_sc(socket, path, func) {
     socket.on(cli_path(path), func);
 }
 
-function promise_api_get_supercollider_volume() {
-    return new Promise(function(resolve, reject) {
-        socket.emit('/node/supercollider', ['volume?']);
-        socket.on('/cli/supercollider', function(msg) {
-            if(msg[0] == "volume") { resolve(msg[1]); }
-        });
+function check_for_promise(resolve, reject, msg, time) {
+    if(!time) time = 2000;
 
-        setTimeout(function() { reject(new Error("get volume timeout!")); }, 1000);
-    });
-}
+    socket.on('/cli/supercollider', function(msg) {
+        if(msg[0] == "state") {
+            var state = JSON.parse(msg[1]);
+            if(!state) {
+                reject(new Error("invalid JSON!"));
+            }
 
-function api_get_supercollider_volume(func) {
-    promise_api_get_supercollider_volume()
-    .then(
-        function(volume){
-            server_volume = volume;
-            if(func) func(volume);
-        },
-        function(error){
-            server_volume = 0;
-            console.log(error.message);
+            resolve(state);
         }
-    );
+    });
+
+    setTimeout(function() { reject(new Error(msg)); }, time);
 }
 
-// returns boot state
-function promise_api_get_supercollider_state() {
+// returns server state
+function promise_get_state() {
     return new Promise(function(resolve, reject) {
         socket.emit('/node/supercollider', ['state?']);
-        socket.on('/cli/supercollider', function(msg) {
-            if(msg[0] == "boot") { resolve(1); }
-            if(msg[0] == "quit") { resolve(0); }
-        });
-
-        setTimeout(function() { reject(new Error("get state timeout!")); }, 1000);
+        check_for_promise(resolve, reject, "get state timeout!");
     });
 }
 
 function api_get_supercollider_state(func) {
-    promise_api_get_supercollider_state()
+    promise_get_state()
     .then(
         function(state){
             server_state = state;
             if(func) func(state);
         },
         function(error){
-            server_state = 0;
-            console.log(error.message);
+            server_state = DEFAULT_STATE;
+            if(func) func(state);
+            console.log("[API]: ", error.message);
         }
     );
 }
 
-function promise_api_mute_supercollider(state) {
+function promise_mute(state) {
     return new Promise(function(resolve, reject) {
         socket.emit('/node/supercollider', ['mute', state]);
-        resolve(state);
+        check_for_promise(resolve, reject, "mute timeout!", 1000);
     });
 }
 
 function api_mute_supercollider(state, func) {
-    promise_api_mute_supercollider(state)
+    promise_mute(state)
     .then(
         function(state) {
             mute_state = state;
@@ -88,42 +75,75 @@ function api_mute_supercollider(state, func) {
         },
         function(error) {
             mute_state = 0;
-            console.log(error.message);
+            if(func) func(state);
+            console.log("[API]: ", error.message);
         }
     );
 }
 
 function api_mute_toggle_supercollider(func) {
-    if(mute_state == 1)
-    api_mute_supercollider(0, func);
-    else
-    api_mute_supercollider(1, func);
+    if(server_state.mute) {
+        api_mute_supercollider(0, func);
+    }    else {
+        api_mute_supercollider(1, func);
+    }
 }
 
 function api_set_supercollider_volume(volume) {
-    socket.emit('/node/supercollider', ['setVolume', volume]);
+    socket.emit('/node/supercollider', ['volume', volume]);
 }
 
-function promise_api_boot_supercollider() {
+function promise_boot_supercollider() {
     return new Promise(function(resolve, reject) {
-        socket.emit('/node/supercollider', ['boot']);
-        socket.on('/cli/supercollider', function(msg) {
-            if(msg[0] == "boot") { resolve(1); }
-        });
-
-        setTimeout(function() { reject(new Error("boot timeout!"));}, 3000);
+        socket.emit('/node/supercollider', ['boot', 1]);
+        check_for_promise(resolve, reject, "boot timeout!", 3000);
     });
 }
 
+function promise_get_booted() {
+    return promise_get_state()
+    .then(
+        function(state){
+            if(!state.boot) {
+                return promise_boot_supercollider();
+            }
+            return DEFAULT_STATE;
+        }
+    );
+}
+
 function api_boot_supercollider(func) {
-    promise_api_get_supercollider_state()
+    promise_get_booted()
+    .then(
+        function(state) {
+            server_state = state;
+            if(func) func(state);
+        },
+        function(error) {
+            server_state = DEFAULT_STATE;
+            if(func) func(state);
+            console.log("[API]: ", error.message);
+        }
+    );
+}
+
+function promise_quit_supercollider() {
+    return new Promise(function(resolve, reject) {
+        socket.emit('/node/supercollider', ['boot', 0]);
+        check_for_promise(resolve, reject, "quit timeout!", 3000);
+    });
+}
+
+function api_quit_supercollider(func) {
+    // 1. know sc state
+    promise_get_state()
     .then(function(state) {
-        if(!state) {
-            return promise_api_boot_supercollider();
-        }
-        else {
-            return 1;
-        }
+        // 2. try to stop record (if started)
+        return promise_record(0);
+    })
+    .then(function(state) {
+        // 3. quit sc
+        return promise_quit_supercollider();
     })
     .then(
         function(state) {
@@ -131,75 +151,99 @@ function api_boot_supercollider(func) {
             if(func) func(state);
         },
         function(error) {
-            server_state = 0;
-            console.log(error.message);
+            server_state = DEFAULT_STATE;
+            console.log("[API]: " + error.message);
         }
     );
 }
 
-function promise_api_quit_supercollider() {
-    return new Promise(function(resolve, reject) {
-        socket.emit('/node/supercollider', ['quit']);
-        socket.on('/cli/supercollider', function(msg) {
-            if(msg[0] == "quit") { resolve(0); }
-        });
+function api_toggle_supercollider(func) {
+    if(server_state.boot) {
+        api_quit_supercollider(func);
+    } else {
+        api_boot_supercollider(func);
+    }
+}
 
-        setTimeout(function() { reject(new Error("quit timeout!")); }, 3000);
+function promise_record(value) {
+    return new Promise(function(resolve, reject) {
+        socket.emit('/node/supercollider', ['record', value]);
+        check_for_promise(resolve, reject, "record timeout!", 2000);
     });
 }
 
-function api_quit_supercollider(func) {
-    promise_api_quit_supercollider()
+function api_record_supercollider(func) {
+    // first try to boot sc
+    promise_get_booted()
+    .then(
+        function(state) { return promise_record(1); }
+    )
     .then(
         function(state) {
             server_state = state;
             if(func) func(state);
         },
         function(error) {
-            server_state = 0;
-            console.log(error.message);
+            server_state = DEFAULT_STATE;
+            if(func) func(state);
+            console.log("[API]: ", error.message);
         }
     );
 }
 
-function api_toggle_supercollider(func) {
-    if(server_state == 1)
-    api_quit_supercollider(func);
-    else
-    api_boot_supercollider(func);
+function api_record_stop_supercollider(func) {
+    // try to stop sc record
+    promise_get_state().
+    then(
+        function(state) {
+            if(state.record) { return promise_record(0); }
+            else { return record; }
+        }
+    )
+    .then(
+        function(state) {
+            rserver_state = state;
+            if(func) func(state);
+        },
+        function(error) {
+            rserver_state = DEFAULT_STATE;
+            if(func) func(state);
+            console.log("[API]: ", error.message);
+        }
+    );
 }
 
-var socket_bindings = {};
-
-function api_bind_action(path, func) {
-    if(socket_bindings[path] === undefined) {
-        socket_bindings[path] = [];
+function api_record_toggle_supercollider(func) {
+    if(server_state.record) {
+        api_record_stop_supercollider(func);
     }
+    else {
+        api_record_supercollider(func);
+    }
+}
 
-    socket_bindings[path].push(func);
+var update_subscribers = [];
+function api_subscribe_update(func) {
+    update_subscribers.push(func);
 }
 
 function init_sockets() {
-    var perform_action = function (name, value) {
-        if(socket_bindings[name]) {
-            socket_bindings[name].forEach(function(func) {
-                func(value);
-            });
-        }
-    };
-
     socket.on('/cli/supercollider', function(msg) {
-        var func;
-        switch(msg[0]) {
-            case 'boot':
-            case 'quit':
-            case 'volume':
-            case 'mute':
-            perform_action('on' + msg[0], msg[1]);
-            break;
-            default: {
-                console.log('[/cli/supercollider] unknown message: ' + msg);
+        if(msg[0] == "state") {
+            var json = JSON.parse(msg[1]);
+            if(!json) {
+                console.log("[API]: invalid JSON data");
+                return;
             }
+            // console.log(json);
+            server_state = json;
+            update_subscribers.forEach(function(func){
+                func(server_state);
+            });
+
+        }
+        else {
+            console.log("[API]: unknown message: " + msg);
         }
     });
 }
@@ -215,15 +259,19 @@ module.exports.from_sc = api_from_sc;
 // supercollider server state control
 module.exports.sc_boot = api_boot_supercollider;
 module.exports.sc_quit = api_quit_supercollider;
+module.exports.sc_boot_toggle = api_toggle_supercollider;
+module.exports.sc_state_request = api_get_supercollider_state;
+
+// record
+module.exports.sc_record = api_record_supercollider;
+module.exports.sc_record_toggle = api_record_toggle_supercollider;
+
 module.exports.sc_mute = api_mute_supercollider;
 module.exports.sc_mute_toggle = api_mute_toggle_supercollider;
-module.exports.sc_toggle = api_toggle_supercollider;
-module.exports.sc_state_request = api_get_supercollider_state;
-module.exports.sc_volume_request = api_get_supercollider_volume;
 module.exports.sc_volume = api_set_supercollider_volume;
 
 // bind actions
-module.exports.bind = api_bind_action;
+module.exports.subscribe = api_subscribe_update;
 
 // utils
 module.exports.cli_path = cli_path;
--- src/js/menu.js
+++ src/js/menu.js
@@ -5,128 +5,96 @@ function nav_menu_log(msg) {
     console.log('[menu.js] ' + msg);
 }
 
+function nav_update(state) {
+    nav_update_boot(state);
+    nav_update_record(state);
+    nav_update_mute(state);
+    nav_update_volume(state);
+}
+
+function nav_update_mute(state) {
+    var el_icon = $("#nav_ui_mute .glyphicon");
+
+    if(state.mute) {
+        el_icon.removeClass("glyphicon-volume-up");
+        el_icon.addClass("glyphicon-volume-off");
+        el_icon.addClass("text-danger");
+    }
+    else {
+        el_icon.removeClass("text-danger");
+        el_icon.removeClass("glyphicon-volume-off");
+        el_icon.addClass("glyphicon-volume-up");
+    }
+}
+
 function nav_menu_init_mute() {
-    var el = $("#nav_ui_mute");
-    var el_icon = el.find(".glyphicon");
+    $("#nav_ui_mute").click(function(){ api.sc_mute_toggle(nav_update_mute); });
+}
 
-    var set_state = function(on) {
-        if(on == 1) {
-            el_icon.removeClass("glyphicon-volume-up");
-            el_icon.addClass("glyphicon-volume-off");
-            el_icon.addClass("text-danger");
-        }
-        else {
-            el_icon.removeClass("text-danger");
-            el_icon.removeClass("glyphicon-volume-off");
-            el_icon.addClass("glyphicon-volume-up");
-        }
-    };
+function nav_update_volume(state) {
+    var el = $('#nav_ui_volume_slider_input');
+    if(el.data("ignore")) return;
 
-    api.bind("onmute", function(state){set_state(state);});
-    el.click(function(){ api.sc_mute_toggle(set_state); });
+    el.slider('setValue', state.volume, false, false);
 }
 
 function nav_menu_init_volume_slider() {
     var el = $('#nav_ui_volume_slider_input');
-    var ignore_events = false;
 
-    function set_volume(v) {
-        if(ignore_events) return;
-        el.slider('setValue', v, false, false);
-        // el.slider('setValue', v, false, false);
-    }
-
-    el.slider({
-        formatter: function(value) { return value + ' db'; }
-    })
-    .on('slideStart', function() {ignore_events = true;})
-    .on('slideStop', function() {ignore_events = false;})
-    .on('slide', function(event) { api.sc_volume(event.value);});
-
-    api.sc_volume_request(set_volume);
-    api.bind("onvolume", set_volume);
+    el.data('ignore', false)
+    .slider({ formatter: function(value) { return value + ' db'; }})
+    .on('slideStart', function() { el.data("ignore", true); })
+    .on('slideStop', function() { el.data("ignore", false); })
+    .on('slide', function(event) { api.sc_volume(event.value); });
 }
 
-function nav_menu_init_record_button() {
-    var el = $("#nav_ui_record");
+function nav_update_record(state) {
     var el_title = $("#nav_ui_record .text");
     var el_icon = $("#nav_ui_record .glyphicon");
 
-    var set_state = function(on) {
-        if(on == 1) {
-            el.data("state", 1);
-            el_title.text("Stop Recording");
-            el_icon.addClass("text-danger");
-        }
-        else {
-            el.data("state", 0);
-            el_title.text("Record");
-            el_icon.removeClass("text-danger");
-        }
-    };
-
-    el.data("state", 0).click(function() {
-        if(el.data("state") == 1) {
-            set_state(0);
-            socket.emit('/node/supercollider', ['stopRecord']);
-        }
-        else {
-            set_state(1);
-            socket.emit('/node/supercollider', ['record']);
-        }
-    });
+    if(state.record) {
+        el_title.text("Stop Recording");
+        el_icon.addClass("text-danger");
+    }
+    else {
+        el_title.text("Record");
+        el_icon.removeClass("text-danger");
+    }
 }
 
-function nav_menu_init_boot_button() {
-    var el = $("#nav_ui_boot");
+function nav_menu_init_record_button() {
+    $("#nav_ui_record").click(function() { api.sc_record_toggle(nav_update_record); });
+}
+
+function nav_update_boot(state) {
     var el_title = $("#nav_ui_boot .text");
     var el_icon = $("#nav_ui_boot .glyphicon");
 
-    var set_state = function(on) {
-        if(on == 1) {
-            el_title.text("Stop supercollider");
-            el_icon.removeClass("glyphicon-off");
-            el_icon.addClass("glyphicon-remove");
-        }
-        else {
-            el_title.text("Boot supercollider");
-            el_icon.removeClass("glyphicon-remove");
-            el_icon.addClass("glyphicon-off");
-        }
-    };
+    if(state.boot) {
+        el_title.text("Stop supercollider");
+        el_icon.removeClass("glyphicon-off");
+        el_icon.addClass("glyphicon-remove");
+    }
+    else {
+        el_title.text("Boot supercollider");
+        el_icon.removeClass("glyphicon-remove");
+        el_icon.addClass("glyphicon-off");
+    }
+}
 
-    el.click(function() { api.sc_toggle(set_state); });
-
-    api.bind("onboot", function(){set_state(1);});
-    api.bind("onquit", function(){set_state(0);});
-    api.sc_state_request(set_state);
+function nav_menu_init_boot_button() {
+    $("#nav_ui_boot").click(function() { api.sc_boot_toggle(nav_update_boot); });
 }
 
 function nav_menu_init(io_socket) {
     socket = io_socket;
+    api.subscribe(nav_update);
     nav_menu_init_mute();
     nav_menu_init_volume_slider();
     nav_menu_init_record_button();
     nav_menu_init_boot_button();
-    nav_menu_handle();
-}
 
-function nav_menu_handle() {
-    // socket.on('/cli/supercollider', function(msg) {
-    //     var func;
-    //     switch(msg[0]) {
-    //         break;
-    //         case 'volume': {
-    //             func = $("#nav_ui_volume_slider_input").data("onchange");
-    //             func(msg[1]);
-    //         }
-    //         break;
-    //         default: {
-    //             console.log('[/cli/supercollider] unknown message: ' + msg);
-    //         }
-    //     }
-    //     nav_menu_log(msg);
-    // });
+    api.sc_state_request();
 }
 
 module.exports.init = nav_menu_init;
