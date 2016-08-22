2dcad8d0a22abbaf70de7be5b03dd7647b93a459
Serge Poltavski <serge.poltavski@gmail.com>
2016-08-23 01:41:05 +0300

    vlabel fixes

--- index.js
+++ index.js
@@ -16,28 +16,34 @@ var OSC_OUT_PORT = OSC_IN_PORT + 1;
 var postmsg = utils.postmsg;
 var postln = utils.postln;
 
-Number.prototype.toHHMMSS = function () {
+Number.prototype.toHHMMSS = function() {
     var seconds = Math.floor(this),
-    hours = Math.floor(seconds / 3600);
-    seconds -= hours*3600;
+        hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
     var minutes = Math.floor(seconds / 60);
-    seconds -= minutes*60;
+    seconds -= minutes * 60;
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds;
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return hours + ':' + minutes + ':' + seconds;
 };
 
 try {
     var oscServer = new osc.Server(OSC_IN_PORT, '0.0.0.0');
     var oscClient = new osc.Client('127.0.0.1', OSC_OUT_PORT);
-} catch(e) {
+} catch (e) {
     throw new Error("Can't start OSC server");
 };
 
 // OSC
-oscServer.on("/sc/stat", function (msg, rinfo) {
+oscServer.on("/sc/stat", function(msg, rinfo) {
     var json = JSON.parse(msg[1]);
     // console.log(json);
     // send to browsers
@@ -49,19 +55,9 @@ oscServer.on("/sc/concert/info", function(msg, rinfo) {
     io.emit("/concert/info", json);
 });
 
-oscServer.on("/sc/vlabel/set", function(msg, rinfo) {
-    postln('label text = "' +  msg[1] + '"');
-    io.emit("/vlabel/set", msg[1]);
-});
-
-oscServer.on("/sc/vlabel/css", function(msg, rinfo) {
-    postln('label css: {' +  msg[1] + ':' + msg[2] + '}');
-    io.emit("/vlabel/css", [msg[1], msg[2]]);
-});
-
 // visual Metronome
 oscServer.on("/sc/vmetro/bar", function(msg, rinfo) {
-    postln('bar = "' +  msg[1] + '"');
+    postln('bar = "' + msg[1] + '"');
     io.emit("/vmetro/bar", msg[1]);
 });
 
@@ -81,7 +77,7 @@ oscServer.on("/sc/vmetro/mark", function(msg, rinfo) {
 });
 
 oscServer.on("/sc/vmetro/css", function(msg, rinfo) {
-    postln('metro css: {' +  msg[1] + ':' + msg[2] + '}');
+    postln('metro css: {' + msg[1] + ':' + msg[2] + '}');
     io.emit("/vmetro/css", [msg[1], msg[2]]);
 });
 
@@ -96,15 +92,17 @@ ui.init(oscServer, oscClient, io);
 // init timer staff
 var serverTimer = new timer.ServerTimer(io, '/server/timer');
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     var addr = socket.request.connection.remoteAddress.substring(7);
+    if(!addr) addr = "127.0.0.1";
+
     postln('connected:    ' + addr);
 
-    socket.on(serverTimer.controlPath, function(msg){
+    socket.on(serverTimer.controlPath, function(msg) {
         timer.control(socket, serverTimer, msg);
     });
 
-    socket.on('/nodejs/info', function(){
+    socket.on('/nodejs/info', function() {
         // send to dedicated client
         io.to(socket.id).emit("/nodejs/info/update", {
             clientsCount: io.engine.clientsCount,
@@ -113,19 +111,19 @@ io.on('connection', function(socket){
     });
 
     server.bindSocket(io, socket, oscClient);
-    
+
     ui.bindClient(socket);
 
     ping.bindSocket(io, socket);
 
-    socket.on('/speakers/test', function(msg){
+    socket.on('/speakers/test', function(msg) {
         // console.log("send " + msg);
         // send to other devices
         socket.broadcast.emit("/speakers/test/update", msg);
         oscClient.send('/nodejs/speaker/control', msg[0], msg[1]);
     });
 
-    socket.on('/info/poll', function(msg){
+    socket.on('/info/poll', function(msg) {
         // send to other clients
         socket.broadcast.emit("/info/poll/update", msg);
         // send to supercollider
@@ -133,22 +131,22 @@ io.on('connection', function(socket){
         oscClient.send('/nodejs/stat/control', msg);
     });
 
-    socket.on("/concert/info/get", function(msg){
+    socket.on("/concert/info/get", function(msg) {
         postln("get info requiest");
         oscClient.send('/concert/info/get', msg);
     });
 
-    socket.on("/concert/control", function(msg){
+    socket.on("/concert/control", function(msg) {
         postln(msg);
         oscClient.send('/concert/' + msg[0], msg[1]);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         postln('disconnected: ' + addr);
     });
 });
 
-http.listen(NODE_PORT, function(){
+http.listen(NODE_PORT, function() {
     postln('listening HTTP on *:' + NODE_PORT);
     postln('listening OSC on *:' + OSC_IN_PORT);
     postln('sending OSC to localhost:' + OSC_OUT_PORT);
--- src/js/app/app_vlabel.js
+++ src/js/app/app_vlabel.js
@@ -1,15 +1,17 @@
 var fittext = require('fittext.js');
 var server = require('../server.js');
+var utils = require('../utils.js');
+var cli_path = utils.cli_path;
 
 function main() {
     $(document).ready(
         function(){
-            server.on('/vlabel/set', function(msg){
+            server.on(cli_path('/vlabel/set'), function(msg) {
                 $("#label").html(msg);
                 $("#label").fitText(0.5);
             });
 
-            server.on('/vlabel/css', function(msg){
+            server.on(cli_path('/vlabel/css'), function(msg) {
                 $("#label").css(msg[0], msg[1]);
             });
             // init
