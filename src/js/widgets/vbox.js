40eb3ba1422588323a348c3d12bee41685427380
Serge Poltavski <serge.poltavski@gmail.com>
2016-08-26 00:16:07 +0300

    vbox align added

--- src/js/widgets/vbox.js
+++ src/js/widgets/vbox.js
@@ -4,6 +4,17 @@ var box = require('./box.js');
 function VBox(params) {
     box.Box.call(this, params);
     this.element.addClass("ui-layout-vbox");
+    switch (params.align) {
+        case "center":
+            this.element.addClass("ui-box-align-center");
+            break;
+        case "right":
+            this.element.addClass("ui-box-align-right");
+            break;
+        case "left":
+            this.element.addClass("ui-box-align-left");
+            break;
+    }
 }
 
 inherits(VBox, box.Box);
